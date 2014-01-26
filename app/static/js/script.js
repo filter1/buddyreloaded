0eba1433c9aefa55a55e4c6193f67e45db00dab3
Johannes Filter <filter@st.ovgu.de>
2014-01-26 15:27:17 +0100

    adding validating with javascript

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -9,4 +9,66 @@ $(document).ready(function() {
     $('#radio_inc').click(function() {
        $('#train').hide();
     });
+
+
+
+    // check input
+    $("#register_button").click(function(){
+      $(".error").hide();
+      var hasError = false;
+      var passwordVal = $("#password").val();
+      var confirmPasswordVal = $("#confirm_password").val();
+      var emailVal = $("#email").val();
+      var nameVal = $("#name").val();
+      var surnameVal = $("#surname").val();
+      var dobVal = $("#dob").val();
+      var privacyVal = $("#privacy").val();
+      var choiceVal = $('input[name="status"]:checked').val();  
+
+      // if (passwordVal == '' || passwordVal.length < 8) {
+      //     $("#password").after('<div class="error">Please enter a password with at least 8 characters long.</div>');
+      //     hasError = true;
+      // } else if (confirmPasswordVal == '') {
+      //     $("#confirm_password").after('<div class="error">Please re-enter your password.</div>');
+      //     hasError = true;
+      // } else if (passwordVal != confirmPasswordVal ) {
+      //     $("#confirm_password").after('<div class="error">Passwords do not match.</div>');
+      //     hasError = true;
+      // } else if ( validateEmail(emailVal) == false) {
+      //     $("#email").after('<div class="error">Please enter an email.</div>');
+      //     hasError = true;
+      // } else if (nameVal == '') {
+      //     $("#name").after('<div class="error">Please enter a name.</div>');
+      //     hasError = true;
+      // } else if (surnameVal == '') {
+      //     $("#surname").after('<div class="error">Please enter a surname.</div>');
+      //     hasError = true;
+      // } else if ( validateDate(dobVal)== false) {
+      //     $("#dob").after('<div class="error">Please enter a valid.</div>');
+      //     hasError = true;
+      // } else if (privacyVal == '') {
+      // 	  $("#privacy").after('<div class="error">Please check privacy.</div>');
+      //     hasError = true;
+      // }
+      if (choiceVal == null) {
+      	  $('input[name="status"]').after('<div class="error">Please pick on.</div>');
+          hasError = true;
+      }
+
+      if(hasError == true) {return false;}
+		});
+
+
+
+
 });
+
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function validateDate(date) { 
+    var re = /^(19|20)\d\d[.](0[1-9]|1[012])[.](0[1-9]|[12][0-9]|3[01])$/;   
+    return re.test(date);
+} 
