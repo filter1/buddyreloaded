400626845863cc9739a4cface34672bb6d1e77c5
Johannes Filter <filter@st.ovgu.de>
2014-01-26 05:22:16 +0100

    ssds

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
  
 class User(db.Model):
   __tablename__ = 'users'
-  uid = db.Column(db.Integer, primary_key = True)
+  uid = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100), nullable=False)
   surname = db.Column(db.String(100), nullable=False)
   email = db.Column(db.String(100), unique=True, nullable=False)
