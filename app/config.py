551a0c15ce910de03fa5751e10ec1ebd909b5628
Johannes Filter <filter@st.ovgu.de>
2014-03-01 04:00:49 +0100

    changing config

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ MAIL_PASSWORD = 'Geyb-Dab-jUd-tAm-foo'
 
 
 #config
-#SQLALCHEMY_DATABASE_URI = 'mysql://buddies:NabnuvwazsuorvAnErc2@localhost/buddies_buddy'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:lollol@localhost/development'
+SQLALCHEMY_DATABASE_URI = 'mysql://buddies:NabnuvwazsuorvAnErc2@localhost/buddies_buddy'
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:lollol@localhost/development'
 
 SECRET_KEY = 'why would I tell you my secret key?'
