fc83241a162aea9d3ecb0a9a60e14e0e0f4d116b
Johannes Filter <filter@st.ovgu.de>
2014-06-13 20:13:12 +0200

    cf

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ MAIL_USERNAME = 'buddies'
 MAIL_PASSWORD = 'Geyb-Dab-jUd-tAm-foo'
 
 
-#SQLALCHEMY_DATABASE_URI = 'mysql://buddies:NabnuvwazsuorvAnErc2@localhost/buddies_buddy'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:lollol@localhost/development'
+SQLALCHEMY_DATABASE_URI = 'mysql://buddies:NabnuvwazsuorvAnErc2@localhost/buddies_buddy'
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:lollol@localhost/development'
 
 SECRET_KEY = 'why would I tell you my secret key?'
